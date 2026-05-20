75b421e540d3a469091153b4bfdfa06931f26964
pranesh-shravtek <pranesh@shravtek.com>
2026-05-20 13:03:32 +0530

    updated the groq api key

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 DATA_API_URL = "https://shrav-company-info-laravel.onrender.com/api/shrav_employees"
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "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"
